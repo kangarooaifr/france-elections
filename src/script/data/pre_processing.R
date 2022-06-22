6118d2e92292ce9ad99d4e729131ab30977af636
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-06-22 12:27:26 +0200

    Prepare for legislatives

--- src/script/data/pre_processing.R
+++ src/script/data/pre_processing.R
@@ -1,32 +1,6 @@
 
 
-#library(data.table)
 library(dplyr)
-
-# data preprocessing
-# merge candidate cols (col to row)
-
-# pre_processing <- function(data, nb_cand, nb_col_before, nb_col_candidate){
-#   
-#   cat("Performing data preprocessing with nb_cand = ", nb_cand, "\n")
-#   
-#   # helper
-#   helper_function <- function(candidate){
-#     
-#     col_start <- (nb_col_before + 1) + nb_col_candidate * (candidate - 1)
-#     col_end <- col_start + (nb_col_candidate - 1)
-#     
-#     data[c(1:nb_col_before, col_start:col_end)]
-#     
-#   }
-#   
-#   # apply helper
-#   list_df <- lapply(1:nb_cand, helper_function)
-#   
-#   # bind rows
-#   data_out <- rbindlist(list_df, use.names = FALSE)
-#   
-# }
 
 
 pre_processing <- function(data, nb_cand, nb_col_before, nb_col_candidate){
