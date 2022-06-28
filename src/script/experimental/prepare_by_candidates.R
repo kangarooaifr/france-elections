53d93e87ab8ae8bc37dcd9d52fa3679c5e98afab
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-06-28 16:30:54 +0200

    Merge raw data preparation

---
+++ src/script/experimental/prepare_by_candidates.R
@@ -0,0 +1,57 @@
+
+
+library(data.table)
+
+
+prepare_by_candidates <- function(data, pattern){
+  
+  cat("-- Prepare dataset by candidate, pattern =", pattern, "\n")
+  
+  # check pattern
+  if(pattern == "Legislatives")
+    COL_CLASSES_CANDIDATES <- COL_CLASSES_CANDIDATES_LEGISLATIVES
+  else
+    COL_CLASSES_CANDIDATES <- COL_CLASSES_CANDIDATES_PRESIDENTIELLE
+  
+  # get values
+  nb_cols_before <- length(COL_CLASSES_BEFORE_CANDIDATES)
+  nb_cols_cand <- length(COL_CLASSES_CANDIDATES)
+  
+  # compute max number of candidates
+  nb_cand <- (dim(data)[2] - nb_cols_before) / nb_cols_cand
+  cat("Nb candidates (max): ", nb_cand, "\n")
+  
+  # helper function
+  helper <- function(x){
+    
+    # compute cols index
+    start <- nb_cols_before + 1 + (x - 1) * nb_cols_cand
+    end <- nb_cols_before + x * nb_cols_cand
+    
+    cols_to_keep <- c(1:nb_cols_before, c(start:end))
+    
+    # subset
+    data[cols_to_keep]
+    
+  }
+  
+  # apply helper and bind result
+  res <- lapply(1:nb_cand, function(x) helper(x))
+  data <- as.data.frame(rbindlist(res, use.names=FALSE))
+  
+  # clean rows with only NA values (because circo have different amount of candidates!)
+  if(pattern == "Legislatives")
+    data <- data[!is.na(data$V.22), ]
+  
+  # set col names
+  colnames(data) <- c(names(COL_CLASSES_BEFORE_CANDIDATES), names(COL_CLASSES_CANDIDATES))
+  
+  # drop cols with % values
+  data <- data[!names(data) %in% RAW_COLS_TO_DROP]
+  
+  cat("Processing done. \n")
+  
+  # return
+  data
+  
+}
---
+++ src/script/experimental/prepare_raw_datasets.R
@@ -0,0 +1,112 @@
+
+
+#' Prepare raw dataset (both presidentielles & legislatives)
+#'
+#' @param new_datasets a character vector containing the names of the files to process
+#' @param pattern a string presidentielles or legislatives (election type)
+#' @param notify a logical, whether progress should be displayed (default = FALSE)
+#' @param session a session object, where to display progress
+#'
+#' @return
+#' @export
+#'
+#' @examples prepare_raw_datasets(c("file_1.csv","file_2.csv"))
+
+
+prepare_raw_datasets <- function(new_datasets, pattern, notify = FALSE, session = NULL){
+  
+  # notify user
+  if(notify)
+    showNotification(
+      ui = "New dataset found, data processing will start.",
+      duration = 5,
+      closeButton = TRUE,
+      type = c("message"),
+      session = session)
+  
+  
+  # get nb dataset to proceed
+  nb_iter <- length(new_datasets)
+  
+  # Set progress bar
+  if(notify)
+    progressSweetAlert(id = "progress", session = session, value = 0, total = 100, display_pct = TRUE, striped = TRUE, 
+                       title = "Début du traitement...")
+  
+  # helper
+  helper <- function(file){
+    
+    cat("Processing file :", file, "\n")
+    
+    # get file index (progress)
+    index <- which(new_datasets == file)
+    start <- (100 / nb_iter) * (index - 1)
+    
+    
+    # -- update progress
+    if(notify)
+      updateProgressBar(session = session,
+                        id = "progress",
+                        value =  start + 10 / nb_iter,
+                        title = paste("Fichier", index, "/", nb_iter, "Chargement des données..."))
+    
+    # -- load file
+    dataset <- load_with_ukn_cols(file.path(path$data_raw, file))
+    
+    if(DEBUG)
+      DEBUG_RAW_DATASET <<- dataset
+    
+    # -- update progress
+    if(notify)
+      updateProgressBar(session = session,
+                        id = "progress",
+                        value =  start + 30 / nb_iter,
+                        title = paste("Fichier", index, "/", nb_iter, "Préparation des données..."))
+    
+    # prepare
+    dataset <- prepare_by_candidates(dataset, pattern)
+    
+    
+    # check drom code ???
+    #dataset <- check_drom_code(dataset)
+    
+    # -- update progress
+    if(notify)
+      updateProgressBar(session = session,
+                        id = "progress",
+                        value =  start + 50 / nb_iter,
+                        title = paste("Fichier", index, "/", nb_iter, "Ajout des codes geo..."))
+    
+    # add codegeo
+    dataset <- add_codgeo(dataset)
+    
+    
+    # -- update progress
+    if(notify)
+      updateProgressBar(session = session,
+                        id = "progress",
+                        value =  start + 70 / nb_iter,
+                        title = paste("Fichier", index, "/", nb_iter, "Sauvegarde (patience)..."))
+    
+    # save
+    save_csv(path = path$data_prepared, file = file, data = dataset)
+    
+    # -- update progress
+    if(notify)
+      updateProgressBar(session = session,
+                        id = "progress",
+                        value =  start + 100 / nb_iter,
+                        title = paste("Fichier", index, "/", nb_iter, "Traitement terminé..."))
+    
+    
+    cat("Processing file done.\n")
+    
+  }
+  
+  # apply helper
+  lapply(new_datasets, function(x) helper(x))
+  
+  if(notify)
+    closeSweetAlert(session)
+  
+}
