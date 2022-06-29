f2d026f6d5b842635b3a7a715d96f2182c8e5b7f
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-06-29 11:11:19 +0200

    Add perfo warning and cleanup

--- src/script/polygon/polygon_server.R
+++ src/script/polygon/polygon_server.R
@@ -31,7 +31,7 @@ polygon_Server <- function(id, r, path) {
     r$geojson <- reactiveVal(NULL)
     
     # Feed this with additional @data cols to use for map
-    r$data_map <- reactiveVal(NULL)
+    #r$data_map <- reactiveVal(NULL)
     
     # Feed with filtered dataset (to display polygons)
     r$filtered_dataset <- reactiveVal(NULL)
@@ -58,16 +58,16 @@ polygon_Server <- function(id, r, path) {
     # to store color palette function
     cache_color_palette <- reactiveVal(NULL)
     
-    filter_by_dep <- reactiveVal(NULL)
-    filter_by_name <- reactiveVal(NULL)
-    color_mode_max <- reactiveVal(NULL)
-    color_mode_min <- reactiveVal(NULL)
-    color_opacity <- reactiveVal(NULL)
+    #filter_by_dep <- reactiveVal(NULL)
+    #filter_by_name <- reactiveVal(NULL)
+    #color_mode_max <- reactiveVal(NULL)
+    #color_mode_min <- reactiveVal(NULL)
+    #color_opacity <- reactiveVal(NULL)
     
-    filtered_map <- reactiveVal(NULL)
+    #filtered_map <- reactiveVal(NULL)
     
     # -- declare objects
-    list_dep <- reactiveVal(NULL)
+    #list_dep <- reactiveVal(NULL)
     
     # list available GeoJsons
     list_geojson <- list("Communes" = "a-com2022.json",
@@ -233,25 +233,26 @@ polygon_Server <- function(id, r, path) {
     # -------------------------------------
     # Observe filters
     # -------------------------------------
-     
+    
+    # display perfo warning
+    observeEvent(input$filter_by_dep, {
+      
+      # warning - geojson
+      output$warning_perfo <- renderUI({
+        
+        # whether or not a geojson is loaded 
+        if("tous" %in% input$filter_by_dep  & geojson_type() == "Communes")
+          box(title = "Message", width = 12, solidHeader = TRUE, status = "warning",
+              "Afficher les communes de tous les départements peut être long...")})
+      
+    })
+    
+    # submit btn
     observeEvent(input$submit_filters, {
 
+      # check
       req(input$filter_by_dep)
 
-      # if(!"Tous" %in% input$filter_by_dep){
-      # 
-      #   tmp_map <- r$data_map()
-      #   tmp_map <- tmp_map[tmp_map@data$dep %in% input$filter_by_dep, ]
-      # 
-      #   filtered_map(tmp_map)
-      # } else {
-      # 
-      #   filtered_map(r$data_map())
-      # 
-      # }
-
-      # *************** CHECK IF ABOVE STILL NEEDED ******************
-      
       # get values
       filter_values <- list(name = input$filter_by_name,
                             dep = input$filter_by_dep)
@@ -275,15 +276,7 @@ polygon_Server <- function(id, r, path) {
         
         else {
           
-          r$pdt_apply_filters(filter_values)
-          
-          # *** TODO: check this for presidential
-          # filter_by_name(input$filter_by_name)
-          # color_mode_max(input$color_mode_max)
-          # color_mode_min(input$color_mode_min)
-          # color_opacity(input$select_opacity)
-          
-          }}
+          r$pdt_apply_filters(filter_values)}}
       
     })
     
@@ -398,85 +391,85 @@ polygon_Server <- function(id, r, path) {
     # Observe
     # -------------------------------------
     
-    observeEvent({filtered_map()
-      filter_by_name()
-      color_mode_max()
-      color_mode_min()
-      color_opacity()}, {
-                    
-      cat("New filtered map is available \n")
-      
-      progressSweetAlert(id = "progress", session = session, value = 10, total = 100, display_pct = TRUE, striped = TRUE, 
-                         title = "Mise à jour de la carte...")
-      
-      filtered_map <- filtered_map()
-      col_name <- filter_by_name()
-      
-      cat("Update map Polygons... \n ")
-      cat("- col_name = ", col_name, "\n")
-      
-
-      cat("Building labels \n")
-      updateProgressBar(session, "progress", value = 20, total = 100, title = "Création des étiquettes...")
-
-      labels <- sprintf(
-        "<strong>%s</strong><br/><br/>%s<br/><br/>%s<br/>%s<br/>%s<br/>%s<br/>%s<br/>%s",
-        filtered_map@data$libgeo, 
-        paste(col_name, ":",
-              filtered_map@data[, (colnames(filtered_map@data) %in% col_name)], "voix,",
-              round(filtered_map@data[, (colnames(filtered_map@data) %in% col_name)] / filtered_map@data$Exprimés * 100, digits = 2), "%"),
-        paste("Inscrits :", filtered_map@data$Inscrits),
-        paste("Abstentions :", filtered_map@data$Abstentions, "(", round(filtered_map@data$Abstentions / filtered_map@data$Inscrits * 100, digits = 2), "% )"),
-        paste("Votants :", filtered_map@data$Votants),
-        paste("Blancs :", filtered_map@data$Blancs),
-        paste("Nuls :", filtered_map@data$Nuls),
-        paste("Exprimés :", filtered_map@data$Exprimés)
-      ) %>% lapply(htmltools::HTML)
-      
-      if(DEBUG)
-        debug_labels <<- labels
-      
-      cat("Building color palette \n")
-      updateProgressBar(session, "progress", value = 40, total = 100, title = "Création de la palette couleur...")
-      
-      # set max value to compute color palette
-      max <- 100
-      if(color_mode_max()){
-        cat("Compute % max \n")
-        #max <- max(filtered_map@data[col_name], na.rm = TRUE)
-        max <- max(filtered_map@data[col_name] / filtered_map@data$Exprimés * 100, na.rm = TRUE)
-      }
-      
-      # set min value to compute color palette
-      min <- 0
-      if(color_mode_min()){
-        cat("Compute % min \n")
-        #max <- max(filtered_map@data[col_name], na.rm = TRUE)
-        min <- min(filtered_map@data[col_name] / filtered_map@data$Exprimés * 100, na.rm = TRUE)
-      }
-    
-      # build color palette  
-      pal <- makePalette(min = min, max = max)
-
-      # update map
-      cat("Add / Update polygons \n")
-      updateProgressBar(session, "progress", value = 50, total = 100, title = "Affichage des contours...")
-      r$proxymap %>%
-        clearGroup("polygons") %>%
-        addPolygons(data = filtered_map, 
-                    weight = 1, 
-                    color = ~pal(filtered_map[[col_name]] / filtered_map@data$Exprimés * 100),
-                    fillColor = ~pal(filtered_map[[col_name]] / filtered_map@data$Exprimés * 100), 
-                    fillOpacity = input$select_opacity / 100,
-                    group = "polygons", 
-                    label = labels,
-                    labelOptions = labelOptions(style = list(
-                                                  "font-size" = "12px")))
-      
-      updateProgressBar(session, "progress", value = 100, total = 100, title = "Terminé")
-      closeSweetAlert(session)
-      
-    })
+    # observeEvent({filtered_map()
+    #   filter_by_name()
+    #   color_mode_max()
+    #   color_mode_min()
+    #   color_opacity()}, {
+    #                 
+    #   cat("New filtered map is available \n")
+    #   
+    #   progressSweetAlert(id = "progress", session = session, value = 10, total = 100, display_pct = TRUE, striped = TRUE, 
+    #                      title = "Mise à jour de la carte...")
+    #   
+    #   filtered_map <- filtered_map()
+    #   col_name <- filter_by_name()
+    #   
+    #   cat("Update map Polygons... \n ")
+    #   cat("- col_name = ", col_name, "\n")
+    #   
+    # 
+    #   cat("Building labels \n")
+    #   updateProgressBar(session, "progress", value = 20, total = 100, title = "Création des étiquettes...")
+    # 
+    #   labels <- sprintf(
+    #     "<strong>%s</strong><br/><br/>%s<br/><br/>%s<br/>%s<br/>%s<br/>%s<br/>%s<br/>%s",
+    #     filtered_map@data$libgeo, 
+    #     paste(col_name, ":",
+    #           filtered_map@data[, (colnames(filtered_map@data) %in% col_name)], "voix,",
+    #           round(filtered_map@data[, (colnames(filtered_map@data) %in% col_name)] / filtered_map@data$Exprimés * 100, digits = 2), "%"),
+    #     paste("Inscrits :", filtered_map@data$Inscrits),
+    #     paste("Abstentions :", filtered_map@data$Abstentions, "(", round(filtered_map@data$Abstentions / filtered_map@data$Inscrits * 100, digits = 2), "% )"),
+    #     paste("Votants :", filtered_map@data$Votants),
+    #     paste("Blancs :", filtered_map@data$Blancs),
+    #     paste("Nuls :", filtered_map@data$Nuls),
+    #     paste("Exprimés :", filtered_map@data$Exprimés)
+    #   ) %>% lapply(htmltools::HTML)
+    #   
+    #   if(DEBUG)
+    #     debug_labels <<- labels
+    #   
+    #   cat("Building color palette \n")
+    #   updateProgressBar(session, "progress", value = 40, total = 100, title = "Création de la palette couleur...")
+    #   
+    #   # set max value to compute color palette
+    #   max <- 100
+    #   if(color_mode_max()){
+    #     cat("Compute % max \n")
+    #     #max <- max(filtered_map@data[col_name], na.rm = TRUE)
+    #     max <- max(filtered_map@data[col_name] / filtered_map@data$Exprimés * 100, na.rm = TRUE)
+    #   }
+    #   
+    #   # set min value to compute color palette
+    #   min <- 0
+    #   if(color_mode_min()){
+    #     cat("Compute % min \n")
+    #     #max <- max(filtered_map@data[col_name], na.rm = TRUE)
+    #     min <- min(filtered_map@data[col_name] / filtered_map@data$Exprimés * 100, na.rm = TRUE)
+    #   }
+    # 
+    #   # build color palette  
+    #   pal <- makePalette(min = min, max = max)
+    # 
+    #   # update map
+    #   cat("Add / Update polygons \n")
+    #   updateProgressBar(session, "progress", value = 50, total = 100, title = "Affichage des contours...")
+    #   r$proxymap %>%
+    #     clearGroup("polygons") %>%
+    #     addPolygons(data = filtered_map, 
+    #                 weight = 1, 
+    #                 color = ~pal(filtered_map[[col_name]] / filtered_map@data$Exprimés * 100),
+    #                 fillColor = ~pal(filtered_map[[col_name]] / filtered_map@data$Exprimés * 100), 
+    #                 fillOpacity = input$select_opacity / 100,
+    #                 group = "polygons", 
+    #                 label = labels,
+    #                 labelOptions = labelOptions(style = list(
+    #                                               "font-size" = "12px")))
+    #   
+    #   updateProgressBar(session, "progress", value = 100, total = 100, title = "Terminé")
+    #   closeSweetAlert(session)
+    #   
+    # })
 
     
     # -------------------------------------
--- src/script/polygon/polygon_ui.R
+++ src/script/polygon/polygon_ui.R
@@ -63,6 +63,17 @@ warning_geojson_UI <- function(id){
   
 }
 
+# -- warning
+warning_perfo_UI <- function(id){
+  
+  # namespace
+  ns <- NS(id)
+  
+  #UI
+  uiOutput(ns("warning_perfo"))
+  
+}
+
 
 # -- filter_by_dep
 filters_UI <- function(id){
@@ -72,35 +83,5 @@ filters_UI <- function(id){
   
   # UI
   uiOutput(ns("filter_panel"))
-  
-  
-  # wellPanel(
-  #   
-  #   # warnings
-  #   uiOutput(ns("is_dataset_loaded")),
-  #   uiOutput(ns("is_geojson_loaded")),
-  #   
-  #   # init (empty choices)
-  #   selectizeInput(ns("filter_by_name"), label = "Candidat", choices = NULL),
-  #   
-  #   # input (empty choices)
-  #   selectizeInput(ns("filter_by_dep"), label = "Département", choices = NULL, multiple = TRUE),
-  #   
-  #   # color palette
-  #   checkboxInput(ns("color_mode_min"), label = "Couleurs : activer % min", value = TRUE, width = NULL),
-  #   checkboxInput(ns("color_mode_max"), label = "Couleurs : activer % max", value = TRUE, width = NULL),
-  #   p("Calcul des couleurs en fonction des % du candidat dans la zone sélectionnée (sinon de 0 à 100%)"),
-  #   
-  #   # opacity
-  #   sliderInput(
-  #     inputId = ns("select_opacity"),
-  #     label = "Opacité",
-  #     min = 0,
-  #     max = 100,
-  #     value = 80,
-  #     step = 5),
-  #   
-  #   actionButton(ns("submit_filters"), label = "Afficher")
-  # )
   
 }
