f8b31b4e64b9ec9d7419c220e804c6191a5aab49
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-06-29 11:10:03 +0200

    Remove old pdt moldule

--- src/script/data/load_with_ukn_cols.R
+++
@@ -1,9 +0,0 @@
-
-
-load_with_ukn_cols <- function(target_file){
-  
-  dat <- read.csv(target_file, col.names = paste("V",1:250), skip = 1, header = F, fill = T, sep = ";", dec = ",")
-  
-  dat <- dat[colSums(!is.na(dat)) > 0]
-  
-}
